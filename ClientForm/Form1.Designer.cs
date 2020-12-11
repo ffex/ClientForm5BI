ebabc018502618a09bad5dfa08ccdbc548fee23b
ffex <kurogan21@gmail.com>
2020-12-11 09:56:38 +0100

    Commit iniziale

---
+++ ClientForm/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIPServer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.btnConnetti = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessaggio = new System.Windows.Forms.TextBox();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.lstMessaggiServ = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Server";
+            // 
+            // txtIPServer
+            // 
+            this.txtIPServer.Location = new System.Drawing.Point(84, 19);
+            this.txtIPServer.Name = "txtIPServer";
+            this.txtIPServer.Size = new System.Drawing.Size(100, 22);
+            this.txtIPServer.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Porta";
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.Location = new System.Drawing.Point(254, 19);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(100, 22);
+            this.txtPorta.TabIndex = 3;
+            // 
+            // btnConnetti
+            // 
+            this.btnConnetti.Location = new System.Drawing.Point(378, 13);
+            this.btnConnetti.Name = "btnConnetti";
+            this.btnConnetti.Size = new System.Drawing.Size(75, 33);
+            this.btnConnetti.TabIndex = 4;
+            this.btnConnetti.Text = "Connetti";
+            this.btnConnetti.UseVisualStyleBackColor = true;
+            this.btnConnetti.Click += new System.EventHandler(this.btnConnetti_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Messaggio:";
+            // 
+            // txtMessaggio
+            // 
+            this.txtMessaggio.Location = new System.Drawing.Point(101, 60);
+            this.txtMessaggio.Name = "txtMessaggio";
+            this.txtMessaggio.Size = new System.Drawing.Size(253, 22);
+            this.txtMessaggio.TabIndex = 6;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Location = new System.Drawing.Point(378, 54);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(75, 32);
+            this.btnInvia.TabIndex = 7;
+            this.btnInvia.Text = "Invia";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            // 
+            // lstMessaggiServ
+            // 
+            this.lstMessaggiServ.FormattingEnabled = true;
+            this.lstMessaggiServ.ItemHeight = 16;
+            this.lstMessaggiServ.Location = new System.Drawing.Point(15, 111);
+            this.lstMessaggiServ.Name = "lstMessaggiServ";
+            this.lstMessaggiServ.Size = new System.Drawing.Size(339, 340);
+            this.lstMessaggiServ.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 468);
+            this.Controls.Add(this.lstMessaggiServ);
+            this.Controls.Add(this.btnInvia);
+            this.Controls.Add(this.txtMessaggio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnetti);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIPServer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIPServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.Button btnConnetti;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMessaggio;
+        private System.Windows.Forms.Button btnInvia;
+        private System.Windows.Forms.ListBox lstMessaggiServ;
+    }
+}
+
---
+++ ClientForm/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        //variabili globali
+        Socket client;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+
+        private void btnConnetti_Click(object sender, EventArgs e)
+        {
+            IPAddress ipaddr = null;
+            int nPort = 0;
+
+            if(!IPAddress.TryParse(txtIPServer.Text,out ipaddr))
+            {
+                //ip address sbagliato
+                MessageBox.Show("Ip vuoto o non valido.","Errore");
+                //return;
+            }
+
+            if(!int.TryParse(txtPorta.Text,out nPort))
+            {
+                MessageBox.Show("Porta vuota o non valida", "Errore");
+            }
+            //Controllo porte
+
+            client.Connect(ipaddr, nPort);
+
+        }
+    }
+}
